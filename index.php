063b1f5850f1dddab4909c941dde57cdb08b48c1
root <root@vultr.guest>
2018-05-20 00:37:04 +0000

    pushing index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+p {
+font-family: Palatino;
+font-size:12; 
+}
+
+
+
+</style>
+</head>
+<h1> Hello World </h1>
+
+<?php 
+
+
+//$html = file_get_contents("https://www.gutenberg.org/files/31469/31469-h/31469-h.htm");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
+$d = new DOMDocument;
+$mock = new DOMDocument;
+$d->loadHTML(file_get_contents("https://www.gutenberg.org/files/31469/31469-h/31469-h.htm"));
+$body = $d->getElementsByTagName('body')->item(0);
+foreach ($body->childNodes as $child){
+    $mock->appendChild($mock->importNode($child, true));
+}
+
+echo $mock->saveHTML();
+
+
+?>
+
+</body>
+</html>
