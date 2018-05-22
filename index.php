71b0bfab34b9b3f1e968d12c3c456c168d379363
Samuel Dotson <samdotson1992@gmail.com>
2018-05-22 07:32:38 -0400

    strip tags

--- index.php
+++ index.php
@@ -16,18 +16,16 @@ ini_set('display_errors', 1);
 
 $d = new DOMDocument;
 $mock = new DOMDocument;
-$d->loadHTML(file_get_contents("https://www.gutenberg.org/files/31469/31469-h/31469-h.htm"));
-$d->strip_tags($d,'<p><div><span><h1><b><hr><img>')
+$contents = file_get_contents("https://www.gutenberg.org/files/31469/31469-h/31469-h.htm")
+$strip = strip_tags($contents,'<p><div><span><h1><b><hr><img>')
+$d->loadHTML($stip);
 $body = $d->getElementsByTagName('body')->item(0);
 foreach ($body->childNodes as $child){
     
     $mock->appendChild($mock->importNode($child, true));
 }
 
-
-
 echo $mock->saveHTML();
-
 
 
 
