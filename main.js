9c08ee1be4361f5ee6dc03b1d3a14372e435af4a
Samuel Dotson <samdotson1992@gmail.com>
2018-05-25 19:55:50 -0400

    remove od js

--- main.js
+++ main.js
@@ -6,16 +6,3 @@ function fontChange(n){
 }
 
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-    var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
