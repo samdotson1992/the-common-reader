49e3563a3ffe10353447f838a8a014bca341444c
Samuel Dotson <samdotson1992@gmail.com>
2018-05-25 19:48:44 -0400

    bootstrapping

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-var font_size = 15;
+var font_size = 18;
 
 function fontChange(n){
     font_size +=n;
      document.getElementById('book').style.fontSize = parseInt(font_size) +"px" ;
+}
+
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
 }
