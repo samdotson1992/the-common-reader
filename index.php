9bd318f09031147dfa4a26949301b8ae294d3f72
Samuel Dotson <samdotson1992@gmail.com>
2018-05-25 19:59:36 -0400

    changing css

--- index.php
+++ index.php
@@ -13,17 +13,16 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">The Common Reader</a>
     </div>
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+      <li class="active"><a href="#">Library</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Background Color<span class="caret"></span></a>
         <ul class="dropdown-menu">
-        <li> <button class ="b" style="background-color:whitesmoke" type="button"
+        <li> <button class ="#" style="background-color:whitesmoke" type="button"
 onclick="document.getElementById('book').style.backgroundColor = 'whitesmoke'"> </button></li>        
-<li> <button class ="b" style="background-color:#efcba7" type="button"
+<li> <button class ="#" style="background-color:#efcba7" type="button"
         onclick="document.getElementById('book').style.backgroundColor = '#efcba7'"> </button></li>
-          <li><a href="#">Page 1-3</a></li>
         </ul>
       </li>
       <li><a href="#">Page 2</a></li>
@@ -33,10 +32,7 @@ onclick="document.getElementById('book').style.backgroundColor = 'whitesmoke'"> 
 </nav>
 
 
-
-       
-
-
+    
 <?php 
 
 //$html = file_get_contents("https://www.gutenberg.org/files/31469/31469-h/31469-h.htm");
