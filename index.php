1c23faf635f7cc69a03525b31028cbf685634e8e
Samuel Dotson <samdotson1992@gmail.com>
2018-05-25 19:50:05 -0400

    bootstrapping

--- index.php
+++ index.php
@@ -3,36 +3,39 @@
 <head>
 <script type="text/javascript" src="main.js"></script>
 <link href="main.css" rel="stylesheet" type="text/css" />
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 <body id = "book" > 
 
-<div class = "container" >
-<ul class="sidenav">
-        <li><a href="#library">Library</a></li>
-        <li><a href="#settings">Settings</a></li>
-
-        <li> <button class ="b" type="button" onclick= "fontChange(-1)" > - </button></li>
-                
-                <li> <button class ="b" type="button" 
-                        onclick= "fontChange(1)" > + </button></li>
-
-        <li> <button class ="b" style="background-color:whitesmoke" type="button" 
-onclick="document.getElementById('book').style.backgroundColor = 'whitesmoke'"> </button></li>
-
-<li> <button class ="b" style="background-color:#efcba7" type="button" 
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+        <li> <button class ="b" style="background-color:whitesmoke" type="button"
+onclick="document.getElementById('book').style.backgroundColor = 'whitesmoke'"> </button></li>        
+<li> <button class ="b" style="background-color:#efcba7" type="button"
         onclick="document.getElementById('book').style.backgroundColor = '#efcba7'"> </button></li>
+          <li><a href="#">Page 1-3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Page 2</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
 
-</ul>
-
-</div>
 
 
-<div class = "container" > 
-</div>
+       
 
-<div class = "container" > 
 
 <?php 
 
@@ -55,7 +58,7 @@ foreach ($body->childNodes as $child){
 echo $mock->saveHTML();
 
 ?>
- </div>
+
 
 <script>
 var pres = document.getElementsByTagName("pre")
