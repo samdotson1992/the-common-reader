5e767f00371986955899f1df47d64f1fb5a05855
Samuel Dotson <samdotson1992@gmail.com>
2018-05-26 11:45:03 -0400

    get element by class jumbotron

--- index.php
+++ index.php
@@ -20,19 +20,19 @@
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Background Color<span class="caret"></span></a>
         <ul class="dropdown-menu">
         <li> <button  href="#" class = "btn-lg btn-block" style="background-color:whitesmoke" type="button"
-onclick="document.getElementById('book').style.backgroundColor = 'whitesmoke'"> </button></li>        
+onclick="document.getElementClassName('jumbotron').style.backgroundColor = 'whitesmoke'"> </button></li>        
 <li> <button  href="#" class ="btn-lg btn-block" style="background-color:#efcba7" type="button"
-        onclick="document.getElementById('book').style.backgroundColor = '#efcba7'"> </button></li>
+        onclick="document.getElementClassName('jumbotron').style.backgroundColor = '#efcba7'"> </button></li>
         </ul>
       </li>
       <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Font Size <span class="caret"></span></a>
         <ul class="dropdown-menu">
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'x-small'" > x-small  </button></li>
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'small'" > small  </button></li>
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'medium'" > medium  </button></li>
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'large'" > large  </button></li>
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'x-large'" > x-large  </button></li>
-          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementById('book').style.fontSize = 'xx-large'" > xx-large  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'x-small'" > x-small  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'small'" > small  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'medium'" > medium  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'large'" > large  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'x-large'" > x-large  </button></li>
+          <li> <button  href="#" class ="btn-md  btn-block" onclick = "document.getElementClassName('jumbotron').style.fontSize = 'xx-large'" > xx-large  </button></li>
         </ul>
       </li>
         </ul>
