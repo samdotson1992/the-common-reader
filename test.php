0f10cedf49ef7d1d567c807eaa068b2585846762
Samuel Dotson <samdotson1992@gmail.com>
2018-05-27 09:08:10 -0400

    testing url

--- test.php
+++ test.php
@@ -5,7 +5,7 @@
 <?php
 $book = $_GET['book'];
 
-echo 'https://www.gutenberg.org/files/' . $book . '/'. $book '-h/'. $book . '-h.htm';
+echo 'https://www.gutenberg.org/files/' . $book . '/'. $book.  '-h/'. $book . '-h.htm';
 
 ?>
 </h1>
