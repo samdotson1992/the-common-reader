620c328d183d728d2db80e23510b9758378e6b32
Samuel Dotson <samdotson1992@gmail.com>
2018-06-04 20:03:49 -0400

    create database

--- test.php
+++ test.php
@@ -6,6 +6,7 @@
  <?php
  ini_set('display_errors', 1);
  error_reporting(E_ALL);
+
 $servername = "localhost";
 $username = "sam";
 $password = "Password1@";
@@ -18,6 +19,15 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+// Create database
+$sql = "CREATE DATABASE test";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
 ?> 
 
 </h1>
