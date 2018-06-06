5ee558b96323e9e00895ab72f933e2f0c9bc858f
Samuel Dotson <samdotson1992@gmail.com>
2018-06-05 21:00:32 -0400

    making table books_info

--- test.php
+++ test.php
@@ -10,15 +10,36 @@
 $servername = "localhost";
 $username = "sam";
 $password = "Password1@";
+$dbname = "books";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
+
+
+// sql to create table
+$sql = "CREATE TABLE books_info (
+    id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(500),
+    number INT(10),
+    cover_url VARCHAR(500),
+    desc VARCHAR(500),
+    gut_rul VARCHAR(500)
+    )";
+    
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+    
+    $conn->close();
 
 // Create database
 $conn->close();
